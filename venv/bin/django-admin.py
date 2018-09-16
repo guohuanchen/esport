a71c92d76c1290ac6480eb4ce2a9b5eb3bf69463
Guohuan Chen <guohuanchen@Guohuans-MacBook-Pro.local>
2018-09-16 17:10:39 -0400

    uploade

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Derrick/Documents/CSC473/webdesignproject/esport_tutor/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
